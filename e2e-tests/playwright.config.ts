6802de7fdd2512b800bf04aca2d19e16899b6dd0
Matias Wargelin <matias.wargelin@kausal.tech>
2025-09-09 15:18:17 +0300

    E2E tests: use base URL

--- e2e-tests/playwright.config.ts
+++ e2e-tests/playwright.config.ts
@@ -1,4 +1,7 @@
-import { defineConfig, devices } from '@playwright/test';
+import {
+  defineConfig,
+  devices,
+} from '@playwright/test';
 
 /**
  * Read environment variables from file.
@@ -35,7 +38,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.TEST_BASE_URL || 'http://localhost:8000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
