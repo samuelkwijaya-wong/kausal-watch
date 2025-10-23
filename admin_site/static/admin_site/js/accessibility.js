e399208ab1b78a15f1c3767fecf69d4a934c0859
Joe Smallwood <joe.smallwood@kausal.tech>
2025-10-23 13:30:34 +0300

    Add alert for saving status

--- admin_site/static/admin_site/js/accessibility.js
+++ admin_site/static/admin_site/js/accessibility.js
@@ -1,12 +1,15 @@
-(function() {
+(function () {
+  const ACTIONS_STATUS_CLASS = 'actions-status';
+  const WAGTAIL_IS_LOADING_ATTRIBUTE = 'data-w-progress-loading-value';
+  const WAGTAIL_LOADING_TEXT_ATTRIBUTE = 'data-w-progress-active-value';
 
   function waitForElements(selector) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       if (document.querySelector(selector)) {
         return resolve(document.querySelectorAll(selector));
       }
 
-      const observer = new MutationObserver(mutations => {
+      const observer = new MutationObserver((mutations) => {
         if (document.querySelector(selector)) {
           observer.disconnect();
           resolve(document.querySelectorAll(selector));
@@ -15,7 +18,7 @@
 
       observer.observe(document.body, {
         childList: true,
-        subtree: true
+        subtree: true,
       });
     });
   }
@@ -29,10 +32,9 @@
       }
       el.setAttribute(
         'aria-describedby',
-        hiddenInput.getAttribute('aria-describedby')
+        hiddenInput.getAttribute('aria-describedby'),
       );
     });
-
   }
 
   function addAttributesToNotificationMessages() {
@@ -48,6 +50,72 @@
       if (container.innerText) {
         container.setAttribute('aria-label', container.innerText);
       }
+    });
+  }
+
+  function addAttributesToPublishActions() {
+    const actionsBar = document.querySelector('.actions');
+
+    if (actionsBar == null) {
+      return;
+    }
+
+    const actionsStatus = document.createElement('div');
+
+    actionsStatus.classList.add(ACTIONS_STATUS_CLASS, 'screen-reader-only');
+    actionsStatus.setAttribute('aria-live', 'assertive');
+    actionsStatus.setAttribute('role', 'status');
+    actionsStatus.setAttribute('aria-atomic', 'true');
+    actionsStatus.innerText = '';
+
+    actionsBar.appendChild(actionsStatus);
+  }
+
+  function setupAriaBusyForProgressButtons() {
+    setTimeout(() => {
+      const status = document.querySelector(`.${ACTIONS_STATUS_CLASS}`);
+      status.textContent = 'Saving...';
+      console.log('Testing Saving alert after 5 seconds');
+
+      setTimeout(() => {
+        status.textContent = '';
+      }, 1000);
+    }, 5000);
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        if (
+          mutation.type === 'attributes' &&
+          mutation.attributeName === WAGTAIL_IS_LOADING_ATTRIBUTE
+        ) {
+          const status = document.querySelector(`.${ACTIONS_STATUS_CLASS}`);
+          const button = mutation.target;
+
+          const isLoading =
+            button.getAttribute(WAGTAIL_IS_LOADING_ATTRIBUTE) === 'true';
+
+          if (isLoading) {
+            const label = button.getAttribute(WAGTAIL_LOADING_TEXT_ATTRIBUTE);
+
+            setTimeout(() => {
+              status.textContent = label || '';
+              status.setAttribute('aria-label', label || '');
+            }, 100);
+          }
+        }
+      });
+    });
+
+    // Observe all buttons with w-progress controller, for example the save and publish buttons
+    const progressButtons = document.querySelectorAll(
+      "[data-controller*='w-progress']",
+    );
+
+    progressButtons.forEach((button) => {
+      observer.observe(button, {
+        attributes: true,
+        attributeFilter: [WAGTAIL_IS_LOADING_ATTRIBUTE],
+      });
     });
   }
 
@@ -67,18 +135,19 @@
     addActivePlanAccessibilityFlagToBodyClass(accessibilityLevel);
     addAttributesToNotificationMessages();
     fixDraftailDescribedByElements();
+    addAttributesToPublishActions();
+    setupAriaBusyForProgressButtons();
   }
 
   const accessibilityScript = document.currentScript;
-  const accessibilityLevel = accessibilityScript.dataset.activePlanAccessibilityLevel;
+  const accessibilityLevel =
+    accessibilityScript.dataset.activePlanAccessibilityLevel;
 
   if (document.readyState === 'loading') {
-    document.addEventListener(
-      'DOMContentLoaded',
-      (e) => injectAccessibilityFixes(accessibilityLevel)
+    document.addEventListener('DOMContentLoaded', (e) =>
+      injectAccessibilityFixes(accessibilityLevel),
     );
     return;
   }
   injectAccessibilityFixes(activePlan);
-
 })();
