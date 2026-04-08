e931cb87dcca05ce1a178cfc4c861ee5df5f3080
Timo Tuominen <tituomin@gmail.com>
2026-04-08 14:23:00 +0300

    Test plan hostnames with "*" in them in get_view_url

--- actions/tests/test_get_view_url.py
+++ actions/tests/test_get_view_url.py
@@ -1,9 +1,4 @@
-"""
-Tests for Plan.get_view_url without wildcard (*) hostname patterns.
-
-These tests verify that the non-wildcard get_view_url behavior is preserved
-when wildcard hostname support is added.
-"""
+"""Tests for Plan.get_view_url."""
 
 from types import SimpleNamespace
 
@@ -140,3 +135,127 @@ class TestGetViewUrlFallback:
     def test_falls_back_with_locale_prefix(self, plan, no_wildcard_domains):
         url = plan.get_view_url(client_url='https://unknown.example.org', active_locale='fi')
         assert url == 'https://myplan.example.com/fi'
+
+
+@pytest.fixture
+def plan_with_country():
+    return PlanFactory.create(
+        site_url='https://myplan.example.com',
+        primary_language='en',
+        other_languages=['fi'],
+        country='FI',
+    )
+
+
+@pytest.fixture(params=['settings', 'request', 'both'], ids=['via_settings', 'via_request', 'via_both'])
+def mid_wildcard_domain(request, settings):
+    """Provide watch.*.dummy.io pattern via settings, request, or both."""
+    retval = {}
+    settings.HOSTNAME_PLAN_DOMAINS = []
+    if request.param in ('settings', 'both'):
+        settings.HOSTNAME_PLAN_DOMAINS = ['watch.*.dummy.io']
+    if request.param in ('request', 'both'):
+        retval.update({'request': SimpleNamespace(wildcard_domains=['watch.*.dummy.io'])})
+    return retval
+
+
+@pytest.fixture(params=['settings', 'request', 'both'], ids=['via_settings', 'via_request', 'via_both'])
+def simple_wildcard_domain(request, settings):
+    """Provide *.dummy.io pattern via settings, request, or both."""
+    retval = {}
+    settings.HOSTNAME_PLAN_DOMAINS = []
+    if request.param in ('settings', 'both'):
+        settings.HOSTNAME_PLAN_DOMAINS = ['*.dummy.io']
+    if request.param in ('request', 'both'):
+        retval.update({'request': SimpleNamespace(wildcard_domains=['*.dummy.io'])})
+    return retval
+
+
+class TestGetViewUrlWithMidWildcardDomain:
+    """Test get_view_url when client_url matches a watch.*.dummy.io pattern."""
+
+    def test_returns_url_with_plan_identifier(self, plan_with_country, mid_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.watch.fi.dummy.io', **mid_wildcard_domain)
+        assert url == f'https://{plan.identifier}.watch.fi.dummy.io'
+
+    def test_preserves_http_scheme(self, plan_with_country, mid_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='http://anything.watch.fi.dummy.io', **mid_wildcard_domain)
+        assert url == f'http://{plan.identifier}.watch.fi.dummy.io'
+
+    def test_preserves_custom_port(self, plan_with_country, mid_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.watch.fi.dummy.io:8080', **mid_wildcard_domain)
+        assert url == f'https://{plan.identifier}.watch.fi.dummy.io:8080'
+
+    def test_strips_default_https_port(self, plan_with_country, mid_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.watch.fi.dummy.io:443', **mid_wildcard_domain)
+        assert url == f'https://{plan.identifier}.watch.fi.dummy.io'
+
+    def test_strips_default_http_port(self, plan_with_country, mid_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='http://anything.watch.fi.dummy.io:80', **mid_wildcard_domain)
+        assert url == f'http://{plan.identifier}.watch.fi.dummy.io'
+
+    def test_adds_locale_prefix(self, plan_with_country, mid_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.watch.fi.dummy.io', active_locale='fi', **mid_wildcard_domain)
+        assert url == f'https://{plan.identifier}.watch.fi.dummy.io/fi'
+
+    def test_no_locale_prefix_for_primary_language(self, plan_with_country, mid_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.watch.fi.dummy.io', active_locale='en', **mid_wildcard_domain)
+        assert url == f'https://{plan.identifier}.watch.fi.dummy.io'
+
+    def test_preserves_country_from_client_url(self, plan_with_country, mid_wildcard_domain):
+        """The country code in the URL comes from the client_url, not the plan."""
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.watch.de.dummy.io', **mid_wildcard_domain)
+        assert url == f'https://{plan.identifier}.watch.de.dummy.io'
+
+
+class TestGetViewUrlWithSimpleWildcardDomain:
+    """Test get_view_url when client_url matches a *.dummy.io pattern."""
+
+    def test_returns_url_with_plan_identifier(self, plan_with_country, simple_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.fi.dummy.io', **simple_wildcard_domain)
+        assert url == f'https://{plan.identifier}.fi.dummy.io'
+
+    def test_preserves_http_scheme(self, plan_with_country, simple_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='http://anything.fi.dummy.io', **simple_wildcard_domain)
+        assert url == f'http://{plan.identifier}.fi.dummy.io'
+
+    def test_preserves_custom_port(self, plan_with_country, simple_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.fi.dummy.io:8080', **simple_wildcard_domain)
+        assert url == f'https://{plan.identifier}.fi.dummy.io:8080'
+
+    def test_strips_default_https_port(self, plan_with_country, simple_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.fi.dummy.io:443', **simple_wildcard_domain)
+        assert url == f'https://{plan.identifier}.fi.dummy.io'
+
+    def test_strips_default_http_port(self, plan_with_country, simple_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='http://anything.fi.dummy.io:80', **simple_wildcard_domain)
+        assert url == f'http://{plan.identifier}.fi.dummy.io'
+
+    def test_adds_locale_prefix(self, plan_with_country, simple_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.fi.dummy.io', active_locale='fi', **simple_wildcard_domain)
+        assert url == f'https://{plan.identifier}.fi.dummy.io/fi'
+
+    def test_no_locale_prefix_for_primary_language(self, plan_with_country, simple_wildcard_domain):
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.fi.dummy.io', active_locale='en', **simple_wildcard_domain)
+        assert url == f'https://{plan.identifier}.fi.dummy.io'
+
+    def test_preserves_country_from_client_url(self, plan_with_country, simple_wildcard_domain):
+        """The country code in the URL comes from the client_url, not the plan."""
+        plan = plan_with_country
+        url = plan.get_view_url(client_url='https://anything.de.dummy.io', **simple_wildcard_domain)
+        assert url == f'https://{plan.identifier}.de.dummy.io'
